8ac4f59150626d7ff94c496b244a3e76ee72f3d2
Fabio Alejandro Gomez Diaz <fabiongo123@hotmail.com>
2013-09-21 14:18:19 -0500

    Added comment

---
+++ appTestMultipleDevelopers/appTestMultipleDevelopers/appTestMultipleDevelopersAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  appTestMultipleDevelopersAppDelegate.h
+//  appTestMultipleDevelopers
+//
+//  Created by Fabio Gomez on 9/21/13.
+//  Copyright (c) 2013 LION Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface appTestMultipleDevelopersAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ appTestMultipleDevelopers/appTestMultipleDevelopers/appTestMultipleDevelopersViewController.m
@@ -0,0 +1,29 @@
+//
+//  appTestMultipleDevelopersViewController.m
+//  appTestMultipleDevelopers
+//
+//  Created by Fabio Gomez on 9/21/13.
+//  Copyright (c) 2013 LION Mobile. All rights reserved.
+//
+
+#import "appTestMultipleDevelopersViewController.h"
+
+@interface appTestMultipleDevelopersViewController ()
+
+@end
+
+@implementation appTestMultipleDevelopersViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
